ddc28ebd974e8dfa7db702ffac89056bd95bdac7
marshallhch <marshallhch@gmail.com>
2021-05-31 16:36:08 +0900

    05/31

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const conTxt = document.querySelectorAll('.con-txt p a');
+
+conTxt.forEach(element => {
+  const cutTxt = element.textContent.slice(0, 10) + "...";
+  element.textContent = cutTxt;
+});
+
